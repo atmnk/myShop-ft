774b149569251ae730e736c704bc39b632f94af1
Atmaram Naik <naik.atmaram@gmail.com>
2019-01-09 17:55:56 +0530

    ATM | facade pattern base code

--- src/test/java/com/vodqa/ft/tests/PurchaseTest.java
+++ src/test/java/com/vodqa/ft/tests/PurchaseTest.java
@@ -1,33 +1,92 @@
 package com.vodqa.ft.tests;
 
-import com.vodqa.ft.facade.PurchaseFacade;
 import com.vodqa.ft.model.ShippingInfo;
+import com.vodqa.ft.pages.*;
+import com.vodqa.ft.pages.factory.PageFactory;
+import com.vodqa.ft.strategy.CalulationService;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Arrays;
+
 public class PurchaseTest {
 
     @Test
     public void firstTest(){
         WebDriver driver=new ChromeDriver();
-        PurchaseFacade purchaseFacade=new PurchaseFacade(driver);
         ShippingInfo shippingInfo=new ShippingInfo();
         shippingInfo.setLastName("Naik");
         shippingInfo.setFirstName("Atmaram");
         shippingInfo.setCountry("China");
-        purchaseFacade.purchaseItem(0,"M","Green","5400",shippingInfo);
+        int item=0;
+        String size="M";
+        String color="Green";
+        String price="5400";
+
+        ProductsPage productsPage= PageFactory.resolve(driver, ProductsPage.class);
+        ProductPage productPage=PageFactory.resolve(driver, ProductPage.class);
+        ReviewOrderPage reviewOrderPage=PageFactory.resolve(driver, ReviewOrderPage.class);
+        ShippingInfoPage shippingInfoPage=PageFactory.resolve(driver, ShippingInfoPage.class);
+        SignInPage signInPage=PageFactory.resolve(driver, SignInPage.class);
+        productsPage
+                .navigate();
+
+        productsPage.validate.Categories(Arrays.asList("All","Food","Fruits","Electronics","Clothes Men","Clothes Women","Drinks","Groceries"));
+        productsPage.clickNthProduct(item);
+        productPage.setSize(size);
+        productPage.setColor(color);
+        productPage.validate.Price(price);
+        productPage.buy();
+        signInPage.continueAsGuest();
+        shippingInfoPage.setShippingAndContinue(shippingInfo);
+        reviewOrderPage.validate.ItemPrice(String.format("%.2f",Double.parseDouble(price)));
+        reviewOrderPage.validate.GiftPackingPrice(String.format("%.2f", CalulationService.calculateGift(Double.parseDouble(price))));
+        reviewOrderPage.validate.ShippingPrice(String.format("%.2f", CalulationService.calculateShipping(Double.parseDouble(price))));
+        if(shippingInfo.getCountry().equals("India")){
+            reviewOrderPage.validate.TaxPrice(String.format("%.2f", CalulationService.calculateVatTax(Double.parseDouble(price),shippingInfo.getCountry())));
+        } else{
+            reviewOrderPage.validate.TaxPrice(String.format("%.2f", CalulationService.calculateSalesTax(Double.parseDouble(price),shippingInfo.getCountry())));
+        }
         driver.quit();
     }
     @Test
     public void secondTest(){
 
         WebDriver driver=new ChromeDriver();
-        PurchaseFacade purchaseFacade=new PurchaseFacade(driver);
         ShippingInfo shippingInfo=new ShippingInfo();
         shippingInfo.setLastName("Naik");
         shippingInfo.setFirstName("Atmaram");
-        shippingInfo.setCountry("India");
-        purchaseFacade.purchaseItem(0,"L","Blue","9600",shippingInfo);
+        shippingInfo.setCountry("China");
+        int item=0;
+        String size="L";
+        String color="Blue";
+        String price="9600";
+
+        ProductsPage productsPage= PageFactory.resolve(driver, ProductsPage.class);
+        ProductPage productPage=PageFactory.resolve(driver, ProductPage.class);
+        ReviewOrderPage reviewOrderPage=PageFactory.resolve(driver, ReviewOrderPage.class);
+        ShippingInfoPage shippingInfoPage=PageFactory.resolve(driver, ShippingInfoPage.class);
+        SignInPage signInPage=PageFactory.resolve(driver, SignInPage.class);
+        productsPage
+                .navigate();
+
+        productsPage.validate.Categories(Arrays.asList("All","Food","Fruits","Electronics","Clothes Men","Clothes Women","Drinks","Groceries"));
+        productsPage.clickNthProduct(item);
+        productPage.setSize(size);
+        productPage.setColor(color);
+        productPage.validate.Price(price);
+        productPage.buy();
+        signInPage.continueAsGuest();
+        shippingInfoPage.setShippingAndContinue(shippingInfo);
+        reviewOrderPage.validate.ItemPrice(String.format("%.2f",Double.parseDouble(price)));
+        reviewOrderPage.validate.GiftPackingPrice(String.format("%.2f", CalulationService.calculateGift(Double.parseDouble(price))));
+        reviewOrderPage.validate.ShippingPrice(String.format("%.2f", CalulationService.calculateShipping(Double.parseDouble(price))));
+        if(shippingInfo.getCountry().equals("India")){
+            reviewOrderPage.validate.TaxPrice(String.format("%.2f", CalulationService.calculateVatTax(Double.parseDouble(price),shippingInfo.getCountry())));
+        } else{
+            reviewOrderPage.validate.TaxPrice(String.format("%.2f", CalulationService.calculateSalesTax(Double.parseDouble(price),shippingInfo.getCountry())));
+        }
         driver.quit();
     }
 
