82c438bea9c032f25a797b5ab0aa7895a130f91a
Atmaram Naik <naik.atmaram@gmail.com>
2019-03-14 17:42:37 +0530

    ATM | changed timeouts

--- src/test/java/com/vodqa/ft/pages/BasePage.java
+++ src/test/java/com/vodqa/ft/pages/BasePage.java
@@ -2,14 +2,14 @@ package com.vodqa.ft.pages;
 
 import org.openqa.selenium.WebDriver;
 
-public class BasePage {
+public abstract class BasePage {
     String url;
     WebDriver driver;
     public BasePage navigate(){
         driver.navigate().to(url);
         return null;
     }
-    public BasePage(WebDriver driver) {
+    protected BasePage(WebDriver driver) {
         this.driver=driver;
     }
 }
