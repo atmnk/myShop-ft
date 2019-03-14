591526fcc91ad592dc589a8d37dcecaefc4fe80b
Atmaram Naik <naik.atmaram@gmail.com>
2019-03-14 17:40:26 +0530

    ATM | changed timeouts

--- src/test/java/com/vodqa/ft/helpers/ElementHelpers.java
+++ src/test/java/com/vodqa/ft/helpers/ElementHelpers.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 public class ElementHelpers {
-    private static int TIMEOUT=30;
+    private static int TIMEOUT=180;
     public static WebElement getWebElementSaflyBy(WebDriver driver, By by){
         WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
         wait.until(
